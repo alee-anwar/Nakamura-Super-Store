43a2a528742fe42747bfb5367bfc79a6985aca30
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-10 00:31:33 +0500

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Grid from "@mui/material/Grid";
+import Appbar from "./components/Appbar";
 
 export default function App() {
   return (
     <Grid container>
+      <Appbar />
       <Navbar />
       <Outlet />
     </Grid>
-    
   );
 }
-
