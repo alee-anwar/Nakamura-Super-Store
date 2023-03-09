4a38fc3613c2e2fedf59bb0b9357eb5fd623ceb6
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-10 00:31:29 +0500

    Update index.js

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from './pages/Dashboard';
-import Customers from './pages/Customers';
-import Orders from './pages/Orders';
-import Products from './pages/Products';
-import Reviews from './pages/Reviews';
-import Transactions from './pages/Transactions';
+import Dashboard from "./pages/Dashboard";
+import Customers from "./pages/Customers";
+import Orders from "./pages/Orders";
+import Products from "./pages/Products";
+import Reviews from "./pages/Reviews";
+import Transactions from "./pages/Transactions";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  zIndex: {
+    appBar: 1201,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} >
-        <Route path="dashboard" element={<Dashboard/>} />
-        <Route path="products" element={<Products/>} />
-        <Route path="customers" element={<Customers/>} />
-        <Route path="orders" element={<Orders/>} />
-        <Route path="reviews" element={<Reviews/>} />
-        <Route path="transactions" element={<Transactions/>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-)
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="transactions" element={<Transactions />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
