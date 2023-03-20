9d0589c84a39cd9433e803b9c5ff382dd6bca308
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-20 14:28:37 +0500

    Update Orders.js

--- admin/src/pages/Orders.js
+++ admin/src/pages/Orders.js
@@ -1,11 +1,12 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
-import { titleStyle } from "../pages/Style/titleStyle";
 
 const Orders = () => {
   return (
-    <Grid item xs={8} style={titleStyle.title}>
-      Orders
+    <Grid item xs={8} >
+      {/* <Typography variant="h5" pt={10} pl={5}>
+        Orders
+      </Typography> */}
     </Grid>
   );
 };
