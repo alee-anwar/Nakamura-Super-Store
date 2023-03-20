f9bcb03ea52932d369e408be9a7cc666e537cfde
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-20 14:28:06 +0500

    Update NotificationBell.js

--- admin/src/components/Appbar/NotificationBell.js
+++ admin/src/components/Appbar/NotificationBell.js
@@ -2,19 +2,53 @@ import React from "react";
 import Badge from "@mui/material/Badge";
 import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
 import { IconButton } from "@mui/material";
-import Tooltip from '@mui/material/Tooltip';
+import Tooltip from "@mui/material/Tooltip";
+import NotificationMenu from "./NotificationMenu";
 
-const NotificationBell = ({ iconColor, badgeContent }) => {
-    const newNotification = `You have ${badgeContent} notification`;
-    const noNotification = 'No new Notification'
+const NotificationBell = ({ iconColor }) => {
+  const notifications = [
+    {
+      id: 0,
+      label: "First notifiction",
+    },
+    {
+      id: 1,
+      label: "Second notifiction",
+    },
+  ];
+  const newNotification = `You have ${notifications.length} notification`;
+  const noNotification = "No new Notification";
+  const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    // setAnchorE1(null);
+    setOpen(false);
+  };
   return (
-    <Tooltip title={badgeContent? newNotification : noNotification}>
-    <IconButton color="iconColor">
-      <Badge badgeContent={badgeContent} color="primary">
-        <NotificationsRoundedIcon color="action" />
-      </Badge>
-    </IconButton>
-    </Tooltip>
+    <div>
+      <Tooltip title={notifications.length ? newNotification : noNotification}>
+        <IconButton
+          color={iconColor}
+          onClick={notifications.length ? handleOpen : null}
+          anchorel={anchorEl}
+        >
+          <Badge badgeContent={notifications.length} color="primary">
+            <NotificationsRoundedIcon color="action" />
+          </Badge>
+        </IconButton>
+      </Tooltip>
+      <NotificationMenu
+        open={open}
+        anchorEl={anchorEl}
+        handleClose={handleClose}
+        menuItems={notifications}
+      />
+    </div>
   );
 };
 
