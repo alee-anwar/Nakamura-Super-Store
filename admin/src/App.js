4c9a7036501318de4172968d6e12fc50361eda9b
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-20 14:28:59 +0500

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,15 +1,47 @@
-import * as React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Grid from "@mui/material/Grid";
-import Appbar from "./components/Appbar/Appbar";
+import React from "react";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import Customers from "./pages/Customers";
+import Orders from "./pages/Orders";
+import Products from "./pages/Products";
+import Reviews from "./pages/Reviews";
+import Transactions from "./pages/Transactions";
+import Login from "./pages/Transactions";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-export default function App() {
+const theme = createTheme({
+  zIndex: {
+    appBar: 1201,
+  },
+  typography: {
+    h1: {
+      fontWeight: 600,
+      letterSpacing: 0.5,
+      fontSize: '1.6rem',
+      textTransform: 'capitalize',
+    }
+  }
+});
+function App() {
   return (
-    <Grid container>
-      <Appbar />
-      <Navbar />
-      <Outlet />
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+          <Route path="/" element={<Navigate to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="login" element={<Login/>}/>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
+
+export default App;
