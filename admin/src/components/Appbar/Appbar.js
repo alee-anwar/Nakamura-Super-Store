b16ff04eaf181a4de0a199da2dfbcc7ba79b825b
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-20 14:28:00 +0500

    Update Appbar.js

--- admin/src/components/Appbar/Appbar.js
+++ admin/src/components/Appbar/Appbar.js
@@ -1,11 +1,10 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import Logo from "../../assets/admin-logo.png";
 import { format } from "date-fns";
 import NotificationBell from "./NotificationBell";
 import AccountMenu from "./AccountMenu";
-
 const Appbar = () => {
   const appbarStyles = {
     logo: {
@@ -17,6 +16,7 @@ const Appbar = () => {
       flexGrow: 1,
     },
   };
+
   return (
     <AppBar color="inherit" elevation={0} position="fixed">
       <Toolbar>
@@ -29,9 +29,7 @@ const Appbar = () => {
         <Typography sx={appbarStyles.date} color="textSecondary">
           Today date is {format(new Date(), "do MMMM Y")}
         </Typography>
-        <NotificationBell iconColor="primary" badgeContent={4} />
-    
-        
+        <NotificationBell iconColor="primary" />
         <AccountMenu />
       </Toolbar>
     </AppBar>
