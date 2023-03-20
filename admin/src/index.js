bd820edc887ce483d048fed2a07faf884aabeef6
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-20 14:28:33 +0500

    Update index.js

--- admin/src/index.js
+++ admin/src/index.js
@@ -3,37 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import Orders from "./pages/Orders";
-import Products from "./pages/Products";
-import Reviews from "./pages/Reviews";
-import Transactions from "./pages/Transactions";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme({
-  zIndex: {
-    appBar: 1201,
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="products" element={<Products />} />
-          <Route path="customers" element={<Customers />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="transactions" element={<Transactions />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </ThemeProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
