4ec8eac114f421e421cc123a797d17b8ea352e3b
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-20 14:28:39 +0500

    Update Products.js

--- admin/src/pages/Products.js
+++ admin/src/pages/Products.js
@@ -1,13 +1,16 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
-import { titleStyle } from "../pages/Style/titleStyle";
 const Products = () => {
   return (
-    <Grid item xs={8} style={titleStyle.title}>
-      Products
-      <Button variant="contained">+ Create New</Button>
+    <Grid item xs={8} >
+      {/* <Typography variant="h5" pt={10} pl={5}>
+        Products
+      </Typography>
+      <Button variant="contained">+ Create New</Button> */}
     </Grid>
   );
 };
 
+
 export default Products;
+
