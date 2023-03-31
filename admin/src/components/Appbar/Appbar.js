9e3420585a2f80eaddcf0b4b6796cfaf8673da8e
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-31 17:26:38 +0500

    Update Appbar.js

--- admin/src/components/Appbar/Appbar.js
+++ admin/src/components/Appbar/Appbar.js
@@ -1,32 +1,20 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
-import { Box } from "@mui/system";
 import React from "react";
-import Logo from "../../assets/admin-logo.png";
 import { format } from "date-fns";
 import NotificationBell from "./NotificationBell";
 import AccountMenu from "./AccountMenu";
-const Appbar = () => {
-  const appbarStyles = {
-    logo: {
-      height: 55,
-      paddingLeft: 1,
-    },
+
+const drawerWidth = 225;
+const useStyles = {
     date: {
-      paddingLeft: 13,
       flexGrow: 1,
     },
   };
-
+const Appbar = () => {
   return (
-    <AppBar color="inherit" elevation={0} position="fixed">
+    <AppBar color="inherit" elevation={0} position="fixed" sx={{width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
       <Toolbar>
-        <Box
-          component="img"
-          sx={appbarStyles.logo}
-          alt="Admin logo"
-          src={Logo}
-        />
-        <Typography sx={appbarStyles.date} color="textSecondary">
+        <Typography sx={useStyles.date} noWrap component='div' color="textSecondary">
           Today date is {format(new Date(), "do MMMM Y")}
         </Typography>
         <NotificationBell iconColor="primary" />
