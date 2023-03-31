46a503f965513942c561c4ee7aebf2aba357b6b1
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-31 17:26:56 +0500

    Update Navbar.js

--- admin/src/components/Navbar/Navbar.js
+++ admin/src/components/Navbar/Navbar.js
@@ -8,17 +8,20 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { mainNavbarItems } from "./navbarItems";
-import { useNavigate } from "react-router-dom";
-
+import { useLocation, useNavigate } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+import Logo from "../../assets/admin-logo.png";
+import { Box } from "@mui/material";
 
 const drawerWidth = 256;
-const Navbar = () => {
-  const navigate = useNavigate();
-  const navbarStyles = {
+const useStyles = makeStyles((theme) => {
+  return {
     drawer: {
       width: drawerWidth,
+      flexShrink: 0,
       ".MuiDrawer-paper": {
         width: drawerWidth,
+        boxSizing: 'border-box',
       },
     },
     icons: {
@@ -33,18 +36,47 @@ const Navbar = () => {
       paddingLeft: "0px",
       paddingRight: "0px",
     },
+    active: {
+      background: "#f4f4f4",
+    },
+    logo: {
+      height: 55,
+      justifyContent: "center",
+    },
   };
+});
+
+const Navbar = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div>
-      <Drawer sx={navbarStyles.drawer} variant="permanent" anchor="left">
-        <Toolbar />
+      <Drawer sx={classes.drawer} variant="permanent" anchor="left">
+        <Toolbar sx={{justifyContent: 'center'}}>
+          <Box
+          component="img"
+          className={classes.logo}
+          alt="Admin logo"
+          src={Logo}
+        />
+        </Toolbar>
         <Divider />
         <List>
           {mainNavbarItems.map((item) => (
-            <ListItem key={item.id} sx={navbarStyles.listItem}>
+            <ListItem
+              key={item.id}
+              className={
+                (classes.listItem,
+                location.pathname === item.path ? classes.active : null)
+              }
+            >
               <ListItemButton onClick={() => navigate(item.route)}>
-                <ListItemIcon sx={navbarStyles.icons}>{item.icon}</ListItemIcon>
-                <ListItemText sx={navbarStyles.text} primary={item.label} />
+                <ListItemIcon className={classes.icons}>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText className={classes.text} primary={item.label} />
               </ListItemButton>
             </ListItem>
           ))}
@@ -55,4 +87,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
