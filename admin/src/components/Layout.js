1838b6bafc74fa36f437cbd20b603ca4f8f242e6
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-31 17:26:52 +0500

    Update Layout.js

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 // import { Outlet } from "react-router-dom";
 // import Appbar from "./components/Appbar/Appbar";
 import Appbar from "./Appbar/Appbar";
 import Navbar from "./Navbar/Navbar";
-import Header from "./Header";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+import { Box } from "@mui/material";
 
-const Styles = {
-  page: {
-    background: "#ffffff",
-    width: "100%",
-  },
-  root: {
-    display: "flex",
-  },
-};
 
 export default function Layout({ children }) {
-  const [title, setTitle] = useState(null);
-  const location = useLocation();
-  useEffect(() => {
-    const parsedTitle = location.pathname.replace(/\W/g, " ");
-    setTitle(parsedTitle);
-  }, [location]);
+  // const [title, setTitle] = useState(null);
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   const parsedTitle = location.pathname.replace(/\W/g, " ");
+  //   setTitle(parsedTitle);
+  // }, [location]);
 
   return (
-    <div style={Styles.root}>
-      {/* appbar */}
+    <Box sx={{ display: "flex" }}>
+      {/* <CssBaseline /> */}
+      {/* Appbar */}
+      <Navbar />
       <Appbar />
-      {/* drawer */}
-      <Navbar />\{/* Layout */}
-      <div>
-        <Header title={title} />
-        <div style={Styles.page}>{children}</div>
-      </div>
-    </div>
+      {/* Drawer */}
+
+      <Box
+        component="main"
+        ml={32}
+        mr={5}
+        flexGrow={1}
+        mt={2}
+      >
+        {/* <Header title={title} /> */}
+        <Box pt={1} mr={0}>{children}</Box>
+      </Box>
+    </Box>
   );
 }
