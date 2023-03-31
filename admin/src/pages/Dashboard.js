9d79ce72257bb5cb8f6f27b2cdc7584e10a63a08
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-31 17:27:20 +0500

    Update Dashboard.js

--- admin/src/pages/Dashboard.js
+++ admin/src/pages/Dashboard.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
-import { Container } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+
 const Dashboard = () => {
+  const theme = useTheme();
+
   return (
-    <div>
-      <Container>
-        <Paper>Total Sales</Paper>
-        <Paper>Total Orders</Paper>
-        <Paper>Total Products</Paper>
-      </Container>
-    </div>
+    <Box>
+       <Box sx={ theme.mixins.toolbar}/>
+      <Typography variant="h1" >Dashboard</Typography>
+      <Grid container spacing={4}>
+        <Grid item md={4}>
+          <Paper>Total Sales</Paper>
+        </Grid>
+        <Grid item md={4}>
+          <Paper>Total Sales</Paper>
+        </Grid>
+        <Grid item md={4}>
+          <Paper>Total Sales</Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
