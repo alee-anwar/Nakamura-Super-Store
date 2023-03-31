2cbb9c33c770dbb631953b3d5aa6f6fea26b8cd4
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-03-31 17:26:35 +0500

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -6,17 +6,20 @@ import Orders from "./pages/Orders";
 import Products from "./pages/Products";
 import Reviews from "./pages/Reviews";
 import Transactions from "./pages/Transactions";
-import Login from "./pages/Transactions";
+// import Login from "./pages/Transactions";
+import CreateProduct from "./pages/CreateProduct";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const theme = createTheme({
-  zIndex: {
-    appBar: 1201,
-  },
   typography: {
+      fontFamily: "Nunito",
+      fontWeightLight: 400,
+      fontWeightRegular: 500,
+      fontWeightMedium: 600,
+      fontWeightBold: 700,
     h1: {
-      fontWeight: 600,
+      fontWeight: 700,
       letterSpacing: 0.5,
       fontSize: '1.6rem',
       textTransform: 'capitalize',
@@ -36,7 +39,7 @@ function App() {
           <Route path="orders" element={<Orders />} />
           <Route path="reviews" element={<Reviews />} />
           <Route path="transactions" element={<Transactions />} />
-          <Route path="login" element={<Login/>}/>
+          <Route path="createproduct" element={<CreateProduct/>}/>
           </Routes>
         </Layout>
       </BrowserRouter>
