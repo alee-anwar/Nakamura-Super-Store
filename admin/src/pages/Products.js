48c097484146138e530652c1e9c99bf793db3785
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-02 19:20:21 +0500

    Update Products.js

--- admin/src/pages/Products.js
+++ admin/src/pages/Products.js
@@ -4,18 +4,36 @@ import { useTheme } from "@mui/material/styles";
 import { Box } from "@mui/system";
 import { useNavigate } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
-import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import DotsMenuBtn from "../components/DotsMenuBtn";
+import axios from "axios";
+import moment from 'moment';
 
 const Products = () => {
   const theme = useTheme();
   const navigate = useNavigate();
-  const [data, setData] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/products")
-      .then((res) => res.json())
-      .then((data) => setData(data));
+    // fetch("http://localhost:8000/products")
+    //   .then((res) => res.json())
+    //   .then((data) => setProducts(data));
+    axios
+      .get("http://localhost:8000/products")
+      .then((res) => setProducts(res.data));
   }, []);
+
+  // const handleDelete = async (id) => {
+  //   await fetch("http://localhost:8000/products/" + id, {
+  //     method: "DELETE",
+  //   });
+  //   const newProducts = products.filter((product) => product.id !== id);
+  //   setProducts(newProducts);
+  // };
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:8000/products/${id}`);
+    const newProducts = products.filter((product) => product.id !== id);
+    setProducts(newProducts);
+  };
 
   const columns = [
     { field: "id", headerName: "SKU", width: 100 },
@@ -24,33 +42,39 @@ const Products = () => {
       headerName: "Image",
       width: 120,
       renderCell: (params) => (
-        <img src={params.value} alt="url" style={{ width: 50 }} />
+        <img
+          src={params.value}
+          alt="url"
+          style={{ width: 45, height: 40, border: "5px solid #f7f7f7" }}
+        />
       ),
     },
-    { field: "name", headerName: "Name", width: 250 },
+    { field: "title", headerName: "Name", width: 250 },
     { field: "category", headerName: "Category", width: 150 },
-    { field: "price", headerName: "Price", width: 150 },
+    { field: "price", headerName: "Price", width: 150, valueFormatter: ({ value }) => `Rs ${value}` },
     { field: "status", headerName: "Status", width: 150 },
     { field: "date", headerName: "Date", width: 150 },
     {
       field: "action",
       headerName: "Action",
-      renderCell: (params) => <MoreHorizRoundedIcon />,
+      renderCell: (params) => (
+        <DotsMenuBtn product={params.id} handleDelete={handleDelete} />
+      ),
       width: 150,
     },
   ];
   // const threedots = <MoreHorizRoundedIcon/>;
-  const rows = data.map((row) => ({
+  const rows = products.map((row) => ({
     id: row.id,
     image: row.image,
-    name: row.name,
+    title: row.title,
     category: row.category,
     price: row.price,
     status: row.status,
-    date: row.date,
+    date: moment(row.date).format('DD/MM/YYYY')
   }));
 
-  console.log(data);
+  // console.log(products);
 
   return (
     <Box>
@@ -60,7 +84,7 @@ const Products = () => {
           Products
         </Typography>
         <Button variant="contained" onClick={() => navigate("/createproduct")}>
-          Create Product
+          Add Product
         </Button>
       </Box>
 
@@ -69,7 +93,7 @@ const Products = () => {
           rows={rows}
           columns={columns}
           initialState={{
-            ...data.initialState,
+            ...products.initialState,
             pagination: { paginationModel: { pageSize: 9 } },
           }}
           pageSizeOptions={[9, 18, 36]}
