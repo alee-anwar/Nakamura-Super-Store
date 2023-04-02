1fc14e8c6786be5bde6e2875552195e8fb8012fd
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-02 19:20:28 +0500

    Update Transactions.js

--- admin/src/pages/Transactions.js
+++ admin/src/pages/Transactions.js
@@ -1,13 +1,83 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import { Button, IconButton, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import DotsMenuBtn from "../components/DotsMenuBtn";
 
 const Transactions = () => {
+  const theme = useTheme();
+  // const navigate = useNavigate();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
+
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/products/" + id, {
+      method: "DELETE",
+    });
+    const newOrders = orders.filter((order) => order.id !== id);
+    setOrders(newOrders);
+  };
+
+  const columns = [
+    { field: "id", headerName: "Transaction ID", width: 100 },
+    { field: "status", headerName: "Status", width: 150 },
+    { field: "oid", headerName: "OID", width: 150 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "phone", headerName: "Phone#", width: 150 },
+    { field: "date", headerName: "Date", width: 150 },
+    {
+      field: "action",
+      headerName: "Action",
+      renderCell: (params) => (
+        <DotsMenuBtn product={params.id} handleDelete={handleDelete}/>
+      ),
+      width: 150,
+    },
+  ];
+  // const threedots = <MoreHorizRoundedIcon/>;
+  const rows = orders.map((row) => ({
+    id: row.id,
+    status: row.status,
+    oid: row.oid,
+    name: row.name,
+    phone: row.phone,
+    date: row.date,
+  }));
+
+  // console.log(products);
+
   return (
-    <Grid item xs={8}>
-      {/* <Typography variant="h5" pt={10} pl={5}>
-        Transactions
-      </Typography> */}
-    </Grid>
+    <Box>
+      <Box sx={theme.mixins.toolbar} />
+      <Box sx={{ display: "flex" }} mb={3}>
+        <Typography sx={{ flexGrow: 1 }} variant="h1">
+          Transactions
+        </Typography>
+        <Button variant="contained">
+          Add Transaction
+        </Button>
+      </Box>
+
+      <div style={{ height: "78vh", width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            ...orders.initialState,
+            pagination: { paginationModel: { pageSize: 9 } },
+          }}
+          pageSizeOptions={[9, 18, 36]}
+        />
+      </div>
+    </Box>
   );
 };
 
