95ca07182fc9e6acdc645afd9fffb3297eb14f93
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-03 06:27:45 +0500

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,10 +9,10 @@ import Transactions from "./pages/Transactions";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/Signup";
 import CreateProduct from "./pages/CreateProduct";
-import SignLayout from "./components/SignLayout";
+import LayoutSign from "./components/LayoutSign";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import LayoutContainer from "./components/LayoutContainer";
 const theme = createTheme({
   typography: {
     fontFamily: "Nunito",
@@ -25,7 +25,7 @@ const theme = createTheme({
       letterSpacing: 0.5,
       fontSize: "1.6rem",
       textTransform: "capitalize",
-    },
+    }
   },
 });
 function App() {
@@ -44,18 +44,40 @@ function App() {
   // };
 
   return (
+    // <ThemeProvider theme={theme}>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<LayoutContainer />}>
+    //         <Route element={<LayoutSign />}>
+    //           <Route path="login" element={<SignIn />} />
+    //           <Route path="signup" element={<SignUp />} />
+    //         </Route>
+    //         <Route element={<Layout />}>
+    //           <Route path="/" element={<Navigate to="dashboard" />} />
+    //           <Route path="dashboard" element={<Dashboard />} />
+    //           <Route path="products" element={<Products />} />
+    //           <Route path="customers" element={<Customers />} />
+    //           <Route path="orders" element={<Orders />} />
+    //           <Route path="reviews" element={<Reviews />} />
+    //           <Route path="transactions" element={<Transactions />} />
+    //           <Route path="createproduct" element={<CreateProduct />} />
+    //         </Route>
+    //       </Route>
+    //     </Routes>
+    //   </BrowserRouter>
+    // </ThemeProvider>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         {isLoginPage || isSignupPage ? ( // render the new layout for login and signup pages
-          <SignLayout>
-            <Routes key="sign-layout">
+          <LayoutSign>
+            <Routes>
               <Route path="login" element={<SignIn />} />
               <Route path="signup" element={<SignUp />} />
             </Routes>
-          </SignLayout>
+          </LayoutSign>
         ) : (
           <Layout>
-            <Routes key="main-layout">
+            <Routes>
               <Route path="/" element={<Navigate to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="products" element={<Products />} />
