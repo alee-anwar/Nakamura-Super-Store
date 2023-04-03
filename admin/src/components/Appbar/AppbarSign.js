4117c550d4cc0e5af00051a55520b86bbce4ce2c
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-03 06:27:42 +0500

    Update AppbarSign.js

--- admin/src/components/Appbar/AppbarSign.js
+++ admin/src/components/Appbar/AppbarSign.js
@@ -1,11 +1,34 @@
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/admin-logo.png";
 import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+        main: '#969696',
+        contrastText: '#fff',
+      },
+  },
+});
 
 const AppbarSign = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [clickedButton, setClickedButton] = useState("login");
+
+  const handleLoginClick = () => {
+    setClickedButton("login");
+    navigate("/login");
+  };
+
+  const handleSignupClick = () => {
+    setClickedButton("signup");
+    navigate("/signup");
+  };
   return (
+    <ThemeProvider theme={theme}>
     <AppBar color="inherit">
       <Toolbar>
         <Box sx={{ flexGrow: 1 }}>
@@ -19,10 +42,23 @@ const AppbarSign = () => {
           />
         </Box>
 
-        <Button onClick={() => navigate('/login')}>LogIn</Button>
-        <Button onClick={() => navigate('/signup')}>SignUp</Button>
+        <Button
+          onClick={handleLoginClick}
+          variant="text"
+          color={clickedButton === "login" ? "primary" : "neutral"}
+        >
+          LogIn
+        </Button>
+        <Button
+          onClick={handleSignupClick}
+          variant="text"
+          color={clickedButton === "signup" ? "primary" : "neutral"}
+        >
+          SignUp
+        </Button>
       </Toolbar>
     </AppBar>
+    </ThemeProvider>
   );
 };
 
