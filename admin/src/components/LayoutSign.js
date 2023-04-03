bdc83c0e511eb2c32c80cf103b1bf82fb0d249fb
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-03 06:27:36 +0500

    Create LayoutSign.js

---
+++ admin/src/components/LayoutSign.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import AppbarSign from "./Appbar/AppbarSign";
+import { useTheme } from '@mui/material/styles';
+
+function LayoutSign(props) {
+  const theme = useTheme();
+
+  return (
+    <Box>
+      <AppbarSign/>
+      <Box sx={ theme.mixins.toolbar}/>
+      <Box>{props.children}</Box>
+    </Box>
+  );
+}
+
+export default LayoutSign;
