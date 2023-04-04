d9c3aeeddca03a270ba0ba31efa02ba5a6380c69
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-04 21:58:34 +0500

    Update Signup.js

--- admin/src/pages/Signup.js
+++ admin/src/pages/Signup.js
@@ -1,109 +1,3 @@
-// import * as React from 'react';
-// import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import TextField from '@mui/material/TextField';
-// import Grid from '@mui/material/Grid';
-// import Box from '@mui/material/Box';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import { useNavigate } from 'react-router-dom';
-// import Logo from '../assets/blueicon.png'
-
-// export default function SignUp() {
-//     const navigate = useNavigate();
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
-//     console.log({
-//       email: data.get('email'),
-//       password: data.get('password'),
-//     });
-//   };
-
-//   return (
-//       <Container component="main" maxWidth="xs" style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "75vh",
-//       }}>
-//         <CssBaseline />
-//         <Box
-//           sx={{
-//             // marginTop: 8,
-//             display: 'flex',
-//             flexDirection: 'column',
-//             alignItems: 'center',
-//           }}
-//         >
-//                   <Box component='img' src={Logo} sx={{height: 50, mb: 1}}/>
-//           <Typography variant="h1">
-//             Sign up
-//           </Typography>
-//           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-//             <Grid container spacing={2}>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                 size='small'
-//                 //   autoComplete="given-name"
-//                   name="firstName"
-//                   required
-//                   fullWidth
-//                   id="firstName"
-//                   label="First Name"
-//                 //   autoFocus
-//                 />
-//               </Grid>
-//               <Grid item xs={12} sm={6}>
-//                 <TextField
-//                 size='small'
-//                   required
-//                   fullWidth
-//                   id="lastName"
-//                   label="Last Name"
-//                   name="lastName"
-//                 //   autoComplete="family-name"
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                 size='small'
-//                   required
-//                   fullWidth
-//                   id="email"
-//                   label="Email Address"
-//                   name="email"
-//                 //   autoComplete="email"
-//                 />
-//               </Grid>
-//               <Grid item xs={12}>
-//                 <TextField
-//                 size='small'
-//                   required
-//                   fullWidth
-//                   name="password"
-//                   label="Password"
-//                   type="password"
-//                   id="password"
-//                 //   autoComplete="new-password"
-//                 />
-//               </Grid>
-//             </Grid>
-//             <Button
-//               type="submit"
-//               fullWidth
-//               variant="contained"
-//               sx={{ mt: 3, mb: 2 }}
-//               onClick={() => navigate('/login')}
-//             >
-//               Sign Up
-//             </Button>
-//           </Box>
-//         </Box>
-//       </Container>
-//   );
-// }
-
 import * as React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -116,6 +10,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/blueicon.png";
+import axios from "axios";
 
 const SignUpSchema = Yup.object().shape({
   firstName: Yup.string().required("First name is required"),
@@ -124,31 +19,55 @@ const SignUpSchema = Yup.object().shape({
     .email("Invalid email address")
     .required("Email is required"),
   password: Yup.string().required("Password is required"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
 });
 
 export default function SignUp() {
   const navigate = useNavigate();
   const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [submitting, setSubmitting] = React.useState(false);
+
+  const handleSignUp = async (values) => {
+    try {
+      const response = await axios
+        .post("http://localhost:8000/admin", values)
+        .then((response) => {
+          console.log(response);
+          alert("Signup successful!");
+          // navigate("/login");
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Signup failed.");
+        })
+        .finally(() => {
+          setSubmitting(false);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
     validationSchema: SignUpSchema,
     onSubmit: (values) => {
-      setIsSubmitting(true);
       console.log(values);
+      handleSignUp(values);
+      navigate("/login");
       setTimeout(() => {
         setIsSubmitting(false);
         navigate("/login");
       }, 2000);
     },
-    // onSubmit: (values) => {
-    //   console.log(values);
-    //   navigate("/login");
-    // },
   });
 
   return (
@@ -240,6 +159,23 @@ export default function SignUp() {
                   formik.touched.password && Boolean(formik.errors.password)
                 }
                 helperText={formik.touched.password && formik.errors.password}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                size="small"
+                name="confirmPassword"
+                required
+                fullWidth
+                type="password"
+                id="confirmPassword"
+                label="Confirm Password"
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)
+                }
+                helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
               />
             </Grid>
             <Grid item xs={12}>
