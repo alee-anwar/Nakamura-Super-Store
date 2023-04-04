da1730d44f3408da740ec05a88f83f75584858b8
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-04 21:58:52 +0500

    Update Appbar.js

--- admin/src/components/Appbar/Appbar.js
+++ admin/src/components/Appbar/Appbar.js
@@ -1,15 +1,15 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 import React from "react";
 // import { format } from "date-fns";
 import NotificationBell from "./NotificationBell";
 import AccountMenu from "./AccountMenu";
 
 const drawerWidth = 225;
-const useStyles = {
-    date: {
-      flexGrow: 1,
-    },
-  };
+// const useStyles = {
+//     date: {
+//       flexGrow: 1,
+//     },
+//   };
 const Appbar = () => {
   return (
     <AppBar color="inherit" elevation={0} position="fixed" sx={{width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
