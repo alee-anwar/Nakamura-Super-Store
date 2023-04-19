4cc6b31534bf565454b1338a4ca6281b859864c1
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-19 10:33:10 +0500

    Create allCategories.js

---
+++ shop/src/components/allCategories.js
@@ -0,0 +1,52 @@
+export const categories = [
+    {
+        id: 0,
+        label: 'Groceries',
+        route: 'groceries',
+    },
+    {
+        id: 1,
+        label: 'Household items',
+        route: 'householditems',
+    },
+    {
+        id: 2,
+        label: 'Pet Supplies',
+        route: 'petsupplies',
+    },
+    {
+        id: 3,
+        label: 'Health & wellness',
+        route: 'health&wellness',
+    },
+    {
+        id: 4,
+        label: 'Electronics',
+        route: 'electronics',
+    },
+    {
+        id: 4,
+        label: 'Office & School Supplies',
+        route: 'office&schoolsupplies',
+    },
+    {
+        id: 4,
+        label: 'Clothing & accessories',
+        route: 'clothing&accessories',
+    },
+    {
+        id: 4,
+        label: 'Kitchen & dining',
+        route: 'kitchen&dining',
+    },
+    {
+        id: 4,
+        label: 'Automotive',
+        route: 'automotive',
+    },
+    {
+        id: 4,
+        label: 'Home Decor',
+        route: 'homedecor',
+    },
+]
