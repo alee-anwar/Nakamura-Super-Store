2de55cc93630a54ecc37d21a76e0524dcb747def
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-19 10:33:31 +0500

    Create Home.js

---
+++ shop/src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Banner from '../components/Banner'
+
+const Home = () => {
+  return (
+    <Banner/>
+  )
+}
+
+export default Home
