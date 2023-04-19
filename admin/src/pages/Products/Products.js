ceb3af3b8d7f92489b83354e171050aeb58f2c8b
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-19 10:33:40 +0500

    Update Products.js

--- admin/src/pages/Products/Products.js
+++ admin/src/pages/Products/Products.js
@@ -7,49 +7,75 @@ import { DataGrid } from "@mui/x-data-grid";
 import DotsMenuBtn from "../../components/DotsMenuBtn";
 import axios from "axios";
 import moment from 'moment';
-
 const Products = () => {
   const theme = useTheme();
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [deleted, setDeleted] = useState(false);
 
+  console.log(products);
   useEffect(() => {
     // fetch("http://localhost:8000/products")
     //   .then((res) => res.json())
     //   .then((data) => setProducts(data));
     axios
-      .get("http://localhost:4000/productList/viewProducts")
-      .then((res) => setProducts(res.data));
-  }, []);
-
+      .get("http://localhost:3000/productList/viewProducts", {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err.message))
+    // axios({
+    //   method: 'get',
+    //   url: 'http://localhost:3000/productList/viewProducts',
+    //   // mode: 'no-cors'
+    // }).then(response => {
+    //   setProducts(response.data);
+    // }).catch(error => {
+    //   console.log(error.message);
+    // });
+  }, [deleted]);
+  console.log()
   // const handleDelete = async (id) => {
-  //   await fetch("http://localhost:8000/products/" + id, {
+  //   await fetch("http://localhost:8000/products/" + id, { 
   //     method: "DELETE",
   //   });
   //   const newProducts = products.filter((product) => product.id !== id);
   //   setProducts(newProducts);
   // };
   const handleDelete = async (id) => {
-    await axios.delete(`http://localhost:8000/products/${id}`);
+    await axios.delete(`http://localhost:3000/productList/deleteProduct/${id}`);
+    // console.log(id)
     const newProducts = products.filter((product) => product.id !== id);
     setProducts(newProducts);
+    setDeleted(true);
   };
+
+  function ImageRenderer({ value }) {
+    // const imageData = base64.toByteArray(value.data);
+    // const imageUrl = `data:${value.contentType};base64,${base64.fromByteArray(imageData)}`;
+    // return <img src={imageUrl} alt="User avatar" width={64} height={64} />;
+  }
   
   const columns = [
     { field: "id", headerName: "SKU", width: 60 },
     {
-      field: "image",
+      field: "image", 
       headerName: "Image",
       width: 80,
-      renderCell: (params) => (
-        <img
-          src={params.value}
-          alt="url"
-          style={{ width: 45, height: 40, border: "5px solid #f7f7f7" }}
-        />
-      ),
+      renderCell: (params) => <ImageRenderer value={params.value} />
+      
+      // (
+
+        // <img
+        //   // src={`data:${params.value.contentType};base64,${params.value.data.toString('base64')}`}
+        //   alt="url"
+        //   style={{ width: 45, height: 40, border: "5px solid #f7f7f7" }}
+        // />
+      // ),
     },
-    { field: "title", headerName: "Name", width: 150 },
+    { field: "productTitle", headerName: "Name", width: 150 },
     { field: "category", headerName: "Category", width: 120 },
     { field: "subcategory", headerName: "Sub Category", width: 130 },
     { field: "price", headerName: "Price", width: 100, valueFormatter: ({ value }) => `Rs ${value}` },
@@ -69,9 +95,9 @@ const Products = () => {
   ];
   // const threedots = <MoreHorizRoundedIcon/>;
   const rows = products.map((row) => ({
-    id: row.id,
-    image: row.image,
-    title: row.title,
+    id: row.SKU,
+    // image: { data: row.image.data, contentType: row.image.contentType },
+    productTitle: row.productTitle,
     category: row.category,
     price: row.price,
     status: row.status,
