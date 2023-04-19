bbe274aeb333f8ffbf1053be3189fa8b0e391bdb
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-19 10:32:33 +0500

    Update Signup.js

--- admin/src/pages/Signup.js
+++ admin/src/pages/Signup.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import Button from "@mui/material/Button";
@@ -11,6 +11,7 @@ import Container from "@mui/material/Container";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/blueicon.png";
 import axios from "axios";
+import { CircularProgress } from "@mui/material";
 
 const SignUpSchema = Yup.object().shape({
   firstName: Yup.string().required("First name is required"),
@@ -26,13 +27,59 @@ const SignUpSchema = Yup.object().shape({
 
 export default function SignUp() {
   const navigate = useNavigate();
-  const [isSubmitting, setIsSubmitting] = React.useState(false);
-  const [submitting, setSubmitting] = React.useState(false);
+  // const [isSubmitting, setIsSubmitting] = React.useState(false);
+  // const [submitting, setSubmitting] = React.useState(false);
+
+  // const [formData, setFormData] = React.useState({
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   password: "",
+  //   confirmPassword: "",
+  // });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  // handleInputChange = (e) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
 
   const handleSignUp = async (values) => {
+    // e.preventDefault();
+    // setIsLoading(true);
+    setErrorMsg("");
+
+    // try {
+      // Check if user already exists
+      // const response = await axios.post('/api/check-email', {
+      //   email: formData.email,
+      // });
+      // if (response.data.message === 'Email already exists') {
+      //   // email already exists, display error message to user
+      // } else {
+      //   // email is available, continue with signup process
+      // }
+
+      // const res = await axios.get(`http://localhost:3000/authUser/signup=${values.email}`);
+      // if (res.data.length > 0) {
+      //   setErrorMsg("User already exists with this email");
+      //   setIsLoading(false);
+      //   return;
+      // }
+      // Create user if not already exists
+    //   await axios.post("http://localhost:3000/authUser/signup", values);
+    //   navigate("/login");
+    // } catch (err) {
+    //   console.error(err);
+    //   setErrorMsg("Signup failed. Please try again later.");
+    // } finally {
+    //   setIsLoading(false);
+    // }
+
     try {
       const response = await axios
-        .post("http://localhost:8000/admin", values)
+        .post("http://localhost:3000/authUser/signup", values)
         .then((response) => {
           console.log(response);
           alert("Signup successful!");
@@ -41,15 +88,16 @@ export default function SignUp() {
         .catch((error) => {
           console.error(error);
           alert("Signup failed.");
+          setIsLoading(true);
         })
         .finally(() => {
-          setSubmitting(false);
+          setIsLoading(true);
+          // setSubmitting(false);
         });
     } catch (error) {
       console.log(error);
     }
-  };
-
+  }
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -61,12 +109,12 @@ export default function SignUp() {
     validationSchema: SignUpSchema,
     onSubmit: (values) => {
       console.log(values);
-      handleSignUp(values);
-      navigate("/login");
-      setTimeout(() => {
-        setIsSubmitting(false);
-        navigate("/login");
-      }, 2000);
+      handleSignUp();
+      // navigate("/login");
+      // setTimeout(() => {
+      // setIsSubmitting(false);
+      // navigate("/login");
+      // }, 2000);
     },
   });
 
@@ -173,9 +221,13 @@ export default function SignUp() {
                 value={formik.values.confirmPassword}
                 onChange={formik.handleChange}
                 error={
-                  formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)
+                  formik.touched.confirmPassword &&
+                  Boolean(formik.errors.confirmPassword)
                 }
-                helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                helperText={
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
+                }
               />
             </Grid>
             <Grid item xs={12}>
@@ -184,10 +236,20 @@ export default function SignUp() {
                 fullWidth
                 variant="contained"
                 sx={{ my: 2 }}
-                disabled={isSubmitting}
+                // disabled={isSubmitting}
+                disabled={isLoading}
               >
-                {isSubmitting ? "Submitting..." : "Sign Up"}
+                {isLoading ? (
+                  <CircularProgress size={24} color="inherit" />
+                ) : (
+                  "Sign Up"
+                )}
               </Button>
+              {errorMsg && (
+                <Typography color="error" variant="subtitle2" gutterBottom>
+                  {errorMsg}
+                </Typography>
+              )}
             </Grid>
           </Grid>
         </Box>
