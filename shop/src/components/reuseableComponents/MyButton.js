f48630c7b5d13e167461dc6bb220d2c6353b15c9
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-27 22:12:42 +0500

    Create MyButton.js

---
+++ shop/src/components/reuseableComponents/MyButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import MuiButton from "@mui/material/Button";
+
+const StyledButton = styled(MuiButton)`
+  && {
+    background-color: 'primary'; 
+    color: #000;
+  }
+`;
+
+function MyButton(props) {
+  return (
+    <StyledButton variant="contained"
+      component={motion.button}
+      initial={{ y: 300, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ delay: 1.4, duration: 1.5, type: "spring", bounce: .6}}
+      {...props}>
+      {props.children}
+    </StyledButton>
+  );
+}
+
+export default MyButton;
