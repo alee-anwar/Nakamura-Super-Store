fe51de518575a20220b959bf8673487365e6f30a
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-27 22:12:35 +0500

    Update SearchBar.js

--- shop/src/components/SearchBar.js
+++ shop/src/components/SearchBar.js
@@ -1,40 +1,38 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
+// import { motion } from "framer-motion";
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
 const NavSearchBar = styled.div`
   position: relative;
-  width: 450px;
-  height: 43px;
+  width: 200px;
+  height: 36px;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  background-color: var(--light-fill);
-  overflow: hidden;
-  border-radius: 10px;
-  flex: 4;
-  transition: all 0.4s ease;
+  flex: 1;
+  transition: all 1s ease;
   &:active {
-    transform: scale(0.9);
-  }
-  &:hover {
-    color: var(--accent-color);
-    background-color: var(--light-accent-color);
+    transform: scale(0.95);
   }
 `;
+
 const SearchInput = styled.input`
   position: relative;
   height: 100%;
   width: 90%;
-  background-color: transparent;
   border: none;
   font-size: var(--text-font);
-  color: var(--dark-text-color);
+  padding-left: 14px;
 `;
 
 const SearchBar = () => {
   return (
-    <div>Searchbar</div>
-  )
-}
+      <NavSearchBar>
+        <SearchRoundedIcon/>
+        <SearchInput placeholder="Search food..." />
+      </NavSearchBar>
+  );
+};
 
 export default SearchBar;
