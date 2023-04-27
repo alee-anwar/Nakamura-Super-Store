f1dd747f27aec7a36563dc1c7d0c1e0b0e27a81b
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-27 22:12:49 +0500

    Create FilterSidebar.js

---
+++ shop/src/components/FilterSidebar.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {
+    Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Paper,
+  Typography,
+} from '@mui/material';
+
+const FilterSidebar = () => {
+  const [selectedDepartments, setSelectedDepartments] = useState([]);
+  const [selectedBrands, setSelectedBrands] = useState([]);
+
+  const handleDepartmentChange = (event) => {
+    const { name } = event.target;
+    setSelectedDepartments((prevSelectedDepartments) => {
+      if (prevSelectedDepartments.includes(name)) {
+        return prevSelectedDepartments.filter((department) => department !== name);
+      } else {
+        return [...prevSelectedDepartments, name];
+      }
+    });
+  };
+
+  const handleBrandChange = (event) => {
+    const { name } = event.target;
+    setSelectedBrands((prevSelectedBrands) => {
+      if (prevSelectedBrands.includes(name)) {
+        return prevSelectedBrands.filter((brand) => brand !== name);
+      } else {
+        return [...prevSelectedBrands, name];
+      }
+    });
+  };
+
+  return (
+    <Paper  elevation={2} width='256px' style={{ padding: '16px', width:'auto' }}>
+    <FormControl component="fieldset" p={2}>
+      <Typography variant="h6">Departments</Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox checked={selectedDepartments.includes('Fruits & Vegetables')} onChange={handleDepartmentChange} name="Fruits & Vegetables" />}
+          label="Fruits & Vegetables"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={selectedDepartments.includes('Pantry')} onChange={handleDepartmentChange} name="Pantry" />}
+          label="Pantry"
+        />
+      </FormGroup>
+      <Typography variant="h6">Brands</Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox checked={selectedBrands.includes('Bare')} onChange={handleBrandChange} name="Bare" />}
+          label="Bare"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={selectedBrands.includes('Bake')} onChange={handleBrandChange} name="Bake" />}
+          label="Bake"
+        />
+      </FormGroup>
+    </FormControl>
+    </Paper>
+  );
+};
+
+export default FilterSidebar;
