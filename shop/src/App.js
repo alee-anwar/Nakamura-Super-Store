811c2ff2c3f134ad235a0a1b19fa1be63edbe644
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-29 07:24:14 +0500

    Update App.js

--- shop/src/App.js
+++ shop/src/App.js
@@ -9,7 +9,6 @@ import { Box } from "@mui/material";
 import Catalogue from "./pages/Catalogue";
 import Wishlist from "./pages/Wishlist";
 
-
 const theme = createTheme({
   palette: {
     primary: {
@@ -18,11 +17,11 @@ const theme = createTheme({
   },
   typography: {
     h1: {
-      fontWeight: 700,
+      fontWeight: 500,
       letterSpacing: 0.5,
       fontSize: "1.7rem",
       textTransform: "capitalize",
-      color: "#262626"
+      color: "#262626",
     },
   },
 });
@@ -30,41 +29,57 @@ const theme = createTheme({
 function App() {
   const [wishlist, setWishlist] = useState([]);
   const [cart, setCart] = useState([]);
+
   const themeSpace = useTheme();
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header cart={cart} wishlist={wishlist}/>
-        <Box mt={11.5}/>
-          <Routes>
-            <Route path="/" element={<Navigate to="home" />} />
-            <Route
-              path="home"
-              element={
-                <Home
-                  cart={cart}
-                  setCart={setCart}
-                  wishlist={wishlist}
-                  setWishlist={setWishlist}
-                />
-              }
-            />
-            <Route
-              path="catalogue" element={<Catalogue/>}
-            />
-            <Route
-              path="wishlist" element={<Wishlist/>}
-            />
-            <Route
-              path="*"
-              element={
-                <div>
-                  <h1>404 Page Not Found</h1>
-                </div>
-              }
-            />
-          </Routes>
-        <Footer/>
+        <Header wishlist={wishlist} />
+        <Box mt={11.5} />
+        <Routes>
+          <Route path="/" element={<Navigate to="home" />} />
+          <Route
+            path="home"
+            element={
+              <Home
+                cart={cart}
+                setCart={setCart}
+                wishlist={wishlist}
+                setWishlist={setWishlist}
+              />
+            }
+          />
+          <Route
+            path="catalogue"
+            element={
+              <Catalogue
+                cart={cart}
+                setCart={setCart}
+                wishlist={wishlist}
+                setWishlist={setWishlist}
+              />
+            }
+          />
+          <Route
+            path="wishlist"
+            element={
+              <Wishlist
+                setCart={setCart}
+                wishlist={wishlist}
+                setWishlist={setWishlist}
+              />
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>404 Page Not Found</h1>
+              </div>
+            }
+          />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
