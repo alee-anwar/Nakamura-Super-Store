0570e64ea07fa5bf9979050a28448bb90d4f4b9f
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-29 07:24:53 +0500

    Create WishlistSection.js

---
+++ shop/src/components/WishlistSection.js
@@ -0,0 +1,36 @@
+import React from "react";
+import WishlistCard from "./WishlistCard";
+import { Container, Grid, Typography } from "@mui/material";
+
+const WishlistSection = ({ wishlist, setWishlist, setCart }) => {
+  // Remove item from wishlist
+  const removeFromWishlist = (item) => {
+    setWishlist((prev) => prev.filter((element) => element.id !== item.id));
+  };
+
+  return (
+    <>
+      <Typography variant="h1" my={2}>Wishlist</Typography>
+      <Grid container>
+        {wishlist.length === 0 ? (
+          <Typography variant="subtitle1">Your wishlist is empty.</Typography>
+        ) : (
+          wishlist.map((item) => (
+            <Grid item xs={12} sm={6} md={4} key={item.id}>
+              <WishlistCard
+                item={item}
+                setCart={setCart}
+                setWishlist={setWishlist}
+                wishlist={wishlist}
+                removeFromWishlist={removeFromWishlist}
+                showRemoveButton={true}
+              />
+            </Grid>
+          ))
+        )}
+      </Grid>
+    </>
+  );
+};
+
+export default WishlistSection;
