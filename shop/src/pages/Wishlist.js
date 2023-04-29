8c2923f9843545757aaffff3258c49dcb6da46be
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-04-29 07:24:56 +0500

    Update Wishlist.js

--- shop/src/pages/Wishlist.js
+++ shop/src/pages/Wishlist.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import BreadcrumbsComponent from "../components/BreadcrumbsComponent";
+import WishlistSection from "../components/WishlistSection";
+import { Container } from "@mui/material";
 
-const Wishlist = () => {
+const Wishlist = ({ setCart, setWishlist, wishlist }) => {
   return (
-    <div>Wishlist</div>
-  )
-}
+    <Container sx={{ pt: 5 }}>
+      <BreadcrumbsComponent name={"Wishlist"} path={"/wishlist"} />
+      <WishlistSection
+        setCart={setCart}
+        wishlist={wishlist}
+        setWishlist={setWishlist}
+      />
+    </Container>
+  );
+};
 
-export default Wishlist
+export default Wishlist;
