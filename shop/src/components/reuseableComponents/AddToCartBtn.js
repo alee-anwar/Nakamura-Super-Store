2a5cbbf8fe6b0ab2551615cd6937121e82d02ad9
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-04 06:22:02 +0500

    Create AddToCartBtn.js

---
+++ shop/src/components/reuseableComponents/AddToCartBtn.js
@@ -0,0 +1,67 @@
+import { Button, Typography } from '@mui/material';
+import React from 'react'
+import ShoppingCartCheckoutRoundedIcon from "@mui/icons-material/ShoppingCartCheckoutRounded";
+
+const AddToCartBtn = ({cartItems, setCartItems, setTotalCost, item, isFullWidth}) => {
+
+    const isItemInCart = cartItems?.some((cartItem) => cartItem._id === item._id);
+    const btnText = isItemInCart ? "Remove" : "add";
+    const handleAddToCart = () => {
+        setCartItems((prevCartItems) => [...prevCartItems, item]);
+        setTotalCost((prevTotalCost) => prevTotalCost + item.price);
+      };
+    
+      const handleRemoveFromCart = () => {
+        setCartItems((prevCartItems) =>
+          prevCartItems.filter((cartItem) => cartItem._id !== item._id)
+        );
+        setTotalCost((prevTotalCost) => prevTotalCost - item.price);
+      };
+
+  return (
+    <Button
+    variant="contained"
+    fullWidth={isFullWidth}
+    // onClick={() => addToCart(item)}
+    onClick={isItemInCart ? handleRemoveFromCart : handleAddToCart}
+    aria-label={isItemInCart ? "Remove from cart" : "Add to cart"}
+    ml={1}
+    sx={{
+      height: 32,
+      "&:hover::before": {
+        opacity: 1,
+      },
+      "&:hover": {
+        backgroundColor: "#ffe033",
+        "& .add": {
+          opacity: 0,
+        },
+        "& .cart": {
+          opacity: 1,
+        },
+      },
+    }}
+  >
+    <Typography
+      variant="body2"
+      className="add"
+      sx={{ fontWeight: 500 }}
+    >
+      {btnText}
+    </Typography>
+    <ShoppingCartCheckoutRoundedIcon
+      className="cart"
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        opacity: 0,
+        transition: "opacity 0.2s ease-in-out",
+      }}
+    />
+  </Button>
+  )
+}
+
+export default AddToCartBtn
