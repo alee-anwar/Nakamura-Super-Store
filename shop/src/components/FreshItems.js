1bb83d43af8a9f4baa20cb9ccb88d38146d2fc09
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-04 06:22:16 +0500

    Update FreshItems.js

--- shop/src/components/FreshItems.js
+++ shop/src/components/FreshItems.js
@@ -5,18 +5,18 @@ import ProductCard from "./reuseableComponents/ProductCard";
 import { ArrowLeft, ArrowRight } from "./reuseableComponents/Arrow";
 import { Box, Button, Typography } from "@mui/material";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
-import './custom.css';
+import "./custom.css";
 const Vegetables = [
   {
-    id: 214795,
+    _id: 214795,
     title: "Ortega Salsa, Garden Vegetable Mild",
-    price: 1000,
+    price: "1000",
     qty: "250g",
     image: "https://spoonacular.com/productImages/214795-312x231.jpeg",
     imageType: "jpeg",
   },
   {
-    id: 72727,
+    _id: 72727,
     title: "Birds Eye C&amp;W Ultimate Southwest Blend, Frozen Vegetables",
     price: 1000,
     qty: "250g",
@@ -24,7 +24,7 @@ const Vegetables = [
     imageType: "jpeg",
   },
   {
-    id: 98162,
+    _id: 98162,
     title: "Birds Eye Mixed Vegetables, 16 oz",
     price: 1000,
     qty: "250g",
@@ -32,7 +32,7 @@ const Vegetables = [
     imageType: "jpeg",
   },
   {
-    id: 13333,
+    _id: 13333,
     title: "Del Monte Mixed Vegetables, 8.25 oz",
     price: 1000,
     qty: "250g",
@@ -40,7 +40,7 @@ const Vegetables = [
     imageType: "jpeg",
   },
   {
-    id: 76119,
+    _id: 76119,
     title: "Del Monte Mixed Vegetables With Potatoes, 29 Oz",
     image: "https://spoonacular.com/productImages/76119-312x231.jpeg",
     imageType: "jpeg",
@@ -48,13 +48,13 @@ const Vegetables = [
     qty: "250g",
   },
   {
-    id: 197185,
+    _id: 197185,
     title: "Birds Eye Mixed Vegetables, Frozen Vegetables, 80 Oz",
     image: "https://spoonacular.com/productImages/197185-312x231.jpeg",
     imageType: "jpeg",
   },
   {
-    id: 72254,
+    _id: 72254,
     price: 100,
     qty: "250g",
     title: "Goya Goya  Mixed Vegetables, 16 oz",
@@ -62,7 +62,7 @@ const Vegetables = [
     imageType: "jpeg",
   },
   {
-    id: 195908,
+    _id: 195908,
     title: "Herdez Mixed Vegetables. Bag",
     price: 100,
     qty: "250g",
@@ -70,37 +70,37 @@ const Vegetables = [
     imageType: "jpeg",
   },
   {
-    id: 215251,
+    _id: 215251,
     title: "Pictsweet Farms® Mixed Vegetables 28 oz. Bag",
     image: "https://spoonacular.com/productImages/215251-312x231.jpeg",
     imageType: "jpeg",
   },
   {
-    id: 46201,
+    _id: 46201,
     title: "Veg-All Original Mixed Vegetables, Canned Vegetables, 15 oz",
     image: "https://spoonacular.com/productImages/46201-312x231.png",
     imageType: "png",
   },
   {
-    id: 194571,
+    _id: 194571,
     title: "Veg-All Original Mixed Vegetables, Canned Vegetables, 29 oz",
     image: "https://spoonacular.com/productImages/194571-312x231.jpeg",
     imageType: "jpeg",
   },
   {
-    id: 1099667,
+    _id: 1099667,
     title: "Marketside Vegetable Medley, 12 oz",
     image: "https://spoonacular.com/productImages/1099667-312x231.jpeg",
     imageType: "jpeg",
   },
   {
-    id: 144438,
+    _id: 144438,
     title: "Birds Eye Pepper Stir Fry Vegetables, Frozen Vegetables, 14 Oz",
     image: "https://spoonacular.com/productImages/144438-312x231.jpeg",
     imageType: "jpeg",
   },
   {
-    id: 639565,
+    _id: 639565,
     title: "(6 Pack) Great Value Mixed Vegetables, 15 Oz",
     image: "https://spoonacular.com/productImages/639565-312x231.jpeg",
     imageType: "jpeg",
@@ -128,7 +128,13 @@ const responsive = {
   },
 };
 
-const FreshItems = ({ cart, setCart, wishlist, setWishlist }) => {
+const FreshItems = ({
+  cartItems,
+  setCartItems,
+  wishlist,
+  setWishlist,
+  setTotalCost,
+}) => {
   return (
     <Box py={2}>
       <Box display="flex">
@@ -165,16 +171,12 @@ const FreshItems = ({ cart, setCart, wishlist, setWishlist }) => {
         {Vegetables.map((item) => {
           return (
             <ProductCard
-              key={item.id}
-              image={item.image}
-              price={item.price}
-              qty={item.qty}
-              title={item.title}
-              cart={cart}
-              setCart={setCart}
-              wishlist={wishlist}
-              setWishlist={setWishlist}
-              item={item}
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+            wishlist={wishlist}
+            setWishlist={setWishlist}
+            item={item}
+            setTotalCost={setTotalCost}
             />
           );
         })}
