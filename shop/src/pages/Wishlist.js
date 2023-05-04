7adb19b4b8bfd591231087f8f9c4c674fac11ece
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-04 06:21:53 +0500

    Update Wishlist.js

--- shop/src/pages/Wishlist.js
+++ shop/src/pages/Wishlist.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BreadcrumbsComponent from "../components/BreadcrumbsComponent";
-import WishlistSection from "../components/WishlistSection";
-import { Container } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
+import WishlistCard from "../components/WishlistCard";
 
-const Wishlist = ({ setCart, setWishlist, wishlist }) => {
+const Wishlist = ({ setCartItems, setWishlist, wishlist, setTotalCost }) => {
+
+  useEffect(() => {
+    const savedWishlistItems = JSON.parse(localStorage.getItem("wishlist"));
+    if (savedWishlistItems) {
+      setWishlist(savedWishlistItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+  }, [wishlist]);
+
   return (
-    <Container sx={{ pt: 5 }}>
+    <Container maxWidth="lg" sx={{ py: 5 }}>
       <BreadcrumbsComponent name={"Wishlist"} path={"/wishlist"} />
-      <WishlistSection
-        setCart={setCart}
-        wishlist={wishlist}
-        setWishlist={setWishlist}
-      />
+      <Typography variant="h1" my={2}>
+        Wishlist
+      </Typography>
+
+      <Grid container>
+        {wishlist.length === 0 ? (
+          <Typography variant="subtitle1">Your wishlist is empty.</Typography>
+        ) : (
+          wishlist.map((item) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={item._id}>
+              <WishlistCard
+                item={item}
+                setCartItems={setCartItems}
+                setWishlist={setWishlist}
+                wishlist={wishlist}
+                setTotalCost={setTotalCost}
+                // removeFromWishlist={removeFromWishlist}
+                // showRemoveButton={true}
+              />
+            </Grid>
+          ))
+        )}
+      </Grid>
     </Container>
   );
 };
