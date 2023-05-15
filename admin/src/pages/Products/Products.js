98aca1d2fc600e66d64ef0c1aca18c12ea55e7dc
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-15 16:49:19 +0500

    Update Products.js

--- admin/src/pages/Products/Products.js
+++ admin/src/pages/Products/Products.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
 import DotsMenuBtn from "../../components/DotsMenuBtn";
 import axios from "axios";
-import moment from 'moment';
+import moment from "moment";
 const Products = () => {
   const theme = useTheme();
   const navigate = useNavigate();
@@ -15,72 +15,57 @@ const Products = () => {
 
   console.log(products);
   useEffect(() => {
-    // fetch("http://localhost:8000/products")
-    //   .then((res) => res.json())
-    //   .then((data) => setProducts(data));
     axios
       .get("http://localhost:3000/productList/viewProducts", {
         headers: {
-          "Content-Type": "multipart/form-data",
+          "Content-Type": "application/json",
         },
       })
       .then((res) => setProducts(res.data))
-      .catch((err) => console.log(err.message))
-    // axios({
-    //   method: 'get',
-    //   url: 'http://localhost:3000/productList/viewProducts',
-    //   // mode: 'no-cors'
-    // }).then(response => {
-    //   setProducts(response.data);
-    // }).catch(error => {
-    //   console.log(error.message);
-    // });
+      .catch((err) => console.log(err.message));
   }, [deleted]);
-  console.log()
-  // const handleDelete = async (id) => {
-  //   await fetch("http://localhost:8000/products/" + id, { 
-  //     method: "DELETE",
-  //   });
-  //   const newProducts = products.filter((product) => product.id !== id);
-  //   setProducts(newProducts);
-  // };
+  // console.log(products);
+
   const handleDelete = async (id) => {
-    await axios.delete(`http://localhost:3000/productList/deleteProduct/${id}`);
-    // console.log(id)
-    const newProducts = products.filter((product) => product.id !== id);
+    await axios.delete(`http://localhost:3000/productList/deleteProduct/${id}`)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err.message));
+    const newProducts = products.filter((product) => product._id !== id)
     setProducts(newProducts);
     setDeleted(true);
   };
 
-  function ImageRenderer({ value }) {
-    // const imageData = base64.toByteArray(value.data);
-    // const imageUrl = `data:${value.contentType};base64,${base64.fromByteArray(imageData)}`;
-    // return <img src={imageUrl} alt="User avatar" width={64} height={64} />;
-  }
-  
   const columns = [
-    { field: "id", headerName: "SKU", width: 60 },
+    { field: "id", headerName: "ID", width: 60 },
+    { field: "sku", headerName: "SKU", width: 60 },
     {
-      field: "image", 
+      field: "image",
       headerName: "Image",
-      width: 80,
-      renderCell: (params) => <ImageRenderer value={params.value} />
-      
-      // (
-
-        // <img
-        //   // src={`data:${params.value.contentType};base64,${params.value.data.toString('base64')}`}
-        //   alt="url"
-        //   style={{ width: 45, height: 40, border: "5px solid #f7f7f7" }}
-        // />
-      // ),
+      width: 70,
+      renderCell: (params) => {
+        return (
+          // console.log(params.row.image)
+          <img
+            src={params.row.image}
+            alt={params.row.productTitle}
+            height= '80%'
+            width= 'auto'
+            style={{border: '5px solid white'}}
+          />
+        );
+      },
     },
-    { field: "productTitle", headerName: "Name", width: 150 },
-    { field: "category", headerName: "Category", width: 120 },
+    { field: "productTitle", headerName: "Name", width: 130},
+    { field: "category", headerName: "Category", width: 130},
     { field: "subcategory", headerName: "Sub Category", width: 130 },
-    { field: "price", headerName: "Price", width: 100, valueFormatter: ({ value }) => `Rs ${value}` },
-    { field: "color", headerName: "Color", width: 100 },
-    { field: "quantity", headerName: "Quantity", width: 100 },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 90,
+      valueFormatter: ({ value }) => `Rs ${value}`,
+    },
+    { field: "color", headerName: "Color", width: 90 },
+    { field: "quantity", headerName: "Quantity", width: 80 },
     { field: "tag", headerName: "Tag", width: 100 },
     { field: "status", headerName: "Status", width: 100 },
     { field: "date", headerName: "Date", width: 100 },
@@ -88,7 +73,7 @@ const Products = () => {
       field: "action",
       headerName: "Action",
       renderCell: (params) => (
-        <DotsMenuBtn product={params.row} handleDelete={handleDelete}/>
+        <DotsMenuBtn product={params.row} handleDelete={handleDelete} />
       ),
       width: 100,
     },
@@ -96,16 +81,17 @@ const Products = () => {
   // const threedots = <MoreHorizRoundedIcon/>;
   const rows = products.map((row) => ({
     id: row._id,
-    // image: { data: row.image.data, contentType: row.image.contentType },
+    sku: row.sku,
+    image: row.image,
     productTitle: row.productTitle,
     category: row.category,
     price: row.price,
     status: row.status,
-    subcategory : row.subcategory,
+    subcategory: row.subcategory,
     color: row.color,
     quantity: row.quantity,
     tag: row.tag,
-    date: moment(row.date).format('DD/MM/YYYY')
+    date: moment(row.date).format("DD/MM/YYYY"),
   }));
 
   // console.log(products);
