c364cbc74fbd1c0ba99b6148d2d716f0a36ef1bf
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-15 16:49:04 +0500

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,77 @@
-import React, { useState } from "react";
+// import React, { useState, useEffect } from "react";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { BrowserRouter , Routes, Route, Navigate } from "react-router-dom";
+// import Layout from "./components/Layout";
+// import Dashboard from "./pages/Dashboard";
+// import Customers from "./pages/Customers";
+// import Orders from "./pages/Orders";
+// import Reviews from "./pages/Reviews";
+// import Transactions from "./pages/Transactions";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/Signup";
+// import CreateProduct from "./pages/Products/CreateProduct";
+// import LayoutSign from "./components/LayoutSign";
+// import MyAccount from "./pages/MyAccount";
+// import Products from "./pages/Products/Products";
+// import EditProduct from "./pages/Products/EditProduct";
+
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: "Nunito",
+//     fontWeightLight: 400,
+//     fontWeightRegular: 500,
+//     fontWeightMedium: 600,
+//     fontWeightBold: 700,
+//     h1: {
+//       fontWeight: 700,
+//       letterSpacing: 0.5,
+//       fontSize: "1.6rem",
+//       textTransform: "capitalize",
+//     },
+//   },
+// });
+
+// function App() {
+//   const currentPath = window.location.pathname;
+//   const isLoginPage = currentPath === "/login";
+//   const isSignupPage = currentPath === "/signup";
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <BrowserRouter>
+//         {isLoginPage || isSignupPage ? ( // render the new layout for login and signup pages
+//           <LayoutSign>
+//             <Routes>
+//               <Route path="login" element={<SignIn />} />
+//               <Route path="signup" element={<SignUp />} />
+//             </Routes>
+//           </LayoutSign>
+//         ) : (
+//         <Layout>
+//           <Routes>
+//             <Route path="/" element={<Navigate to="dashboard" />} />
+//             <Route path="dashboard" element={<Dashboard />} />
+//             <Route path="products" element={<Products />} />
+//             <Route path="customers" element={<Customers />} />
+//             <Route path="orders" element={<Orders />} />
+//             <Route path="reviews" element={<Reviews />} />
+//             <Route path="transactions" element={<Transactions />} />
+//             <Route path="createproduct" element={<CreateProduct />} />
+//             <Route path="myaccount" element={<MyAccount />} />
+//             <Route path="editproduct" element={<EditProduct />} />
+//           </Routes>
+//         </Layout>
+//          )}
+//       </BrowserRouter>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter , Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Customers from "./pages/Customers";
@@ -9,12 +82,9 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/Signup";
 import CreateProduct from "./pages/Products/CreateProduct";
 import LayoutSign from "./components/LayoutSign";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MyAccount from "./pages/MyAccount";
 import Products from "./pages/Products/Products";
 import EditProduct from "./pages/Products/EditProduct";
-import useToken from "./custumHooks/useToken";
 
 const theme = createTheme({
   typography: {
@@ -33,28 +103,17 @@ const theme = createTheme({
 });
 
 function App() {
-  const currentPath = window.location.pathname;
-  const isLoginPage = currentPath === "/login";
-  const isSignupPage = currentPath === "/signup";
-  // const navigate = useNavigate();
-  // const token = localStorage.getItem('token');
-  // const isAuthenticated = token ? true : false;
-  // const { token, setToken } = useToken();
-  // if (!token) {
-  //   return <SignIn setToken={setToken} />;
-  // }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token); // Set isLoggedIn based on whether the token is present
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        {isLoginPage || isSignupPage ? ( // render the new layout for login and signup pages
-          <LayoutSign>
-            <Routes>
-              <Route path="login" element={<SignIn />} />
-              <Route path="signup" element={<SignUp />} />
-            </Routes>
-          </LayoutSign>
-        ) : (
+        {isLoggedIn ? ( // Render the layout and protected routes if the user is logged in
           <Layout>
             <Routes>
               <Route path="/" element={<Navigate to="dashboard" />} />
@@ -66,9 +125,18 @@ function App() {
               <Route path="transactions" element={<Transactions />} />
               <Route path="createproduct" element={<CreateProduct />} />
               <Route path="myaccount" element={<MyAccount />} />
-              <Route path="editproduct" element={<EditProduct />} />
+              <Route path="editproduct/:id" element={<EditProduct />} />
             </Routes>
           </Layout>
+        ) : (
+          <>
+          <LayoutSign>
+            <Routes>
+              <Route path="login" element={<SignIn />} />
+              <Route path="signup" element={<SignUp />} />
+            </Routes>
+          </LayoutSign>
+          </>
         )}
       </BrowserRouter>
     </ThemeProvider>
@@ -76,3 +144,4 @@ function App() {
 }
 
 export default App;
+
