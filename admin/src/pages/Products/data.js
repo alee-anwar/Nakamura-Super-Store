cf9eead9d0813cb40782b0668f7c4967b1ee3324
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-15 16:49:28 +0500

    Create data.js

---
+++ admin/src/pages/Products/data.js
@@ -0,0 +1,153 @@
+export const categories = [
+  {
+    id: 0,
+    value: "Grocery",
+    label: "Grocery",
+  },
+  {
+    id: 1,
+    value: "Fresh Produce",
+    label: "Fresh Produce",
+  },
+  {
+    id: 2,
+    value: "Meat and Seafood",
+    label: "Meat and Seafood",
+  },
+  {
+    id: 3,
+    value: "Dairy and Eggs",
+    label: "Dairy and Eggs",
+  },
+  {
+    id: 4,
+    value: "Frozen Foods:",
+    label: "Frozen Foods:",
+  },
+  {
+    id: 5,
+    value: "Bakery",
+    label: "Bakery",
+  },
+  {
+    id: 6,
+    value: "Deli and Prepared Foods",
+    label: "Deli and Prepared Foods",
+  },
+  {
+    id: 7,
+    value: "Personal Care and Health",
+    label: "Personal Care and Health",
+  },
+  {
+    id: 8,
+    value: "Household Supplies",
+    label: "Household Supplies",
+  },
+  {
+    id: 9,
+    value: "Baby and Child Care",
+    label: "Baby and Child Care",
+  },
+  {
+    id: 10,
+    value: "Home and Kitchen",
+    label: "Home and Kitchen",
+  },
+];
+
+export const subcategories = [
+  { value: "Canned Goods", label: "Canned Goods" },
+  { value: "Baking Supplies", label: "Baking Supplies" },
+  { value: "Snacks and Cookies", label: "Snacks and Cookies" },
+  { value: "Beverages", label: "Beverages" },
+  { value: "Vegetables", label: "Vegetables" },
+  {
+    value: "Frozen Fruits and Vegetables",
+    label: "Frozen Fruits and Vegetables",
+  },
+  { value: "Frozen Itmes", label: "Frozen Meals" },
+  { value: "Bath and Body Products", label: "Bath and Body Products" },
+  { value: " Care Products", label: "Hair Care" },
+  { value: "Cleaning Supplies", label: "Cleaning Supplies" },
+  { value: "Laundry Detergent", label: "Laundry Detergent" },
+  { value: "Stationary", label: "Paper Products" },
+  { value: "Baby Care Products", label: "Baby Care Products" },
+];
+
+//   Sure! Here are a few important categories you typically find in a supermarket or a superstore, along with some subcategories:
+
+// Grocery:
+
+// Canned Goods
+// Pasta and Rice
+// Baking Supplies
+// Condiments and Sauces
+// Breakfast Foods
+// Snacks and Cookies
+// Beverages (Soft Drinks, Juices, Coffee, Tea)
+// Spices and Seasonings
+// Fresh Produce:
+
+// Fruits (Apples, Bananas, Oranges, Berries)
+// Vegetables (Lettuce, Tomatoes, Carrots, Broccoli)
+// Herbs
+// Meat and Seafood:
+
+// Beef
+// Poultry (Chicken, Turkey)
+// Pork
+// Fish and Seafood
+// Dairy and Eggs:
+
+// Milk
+// Cheese
+// Yogurt
+// Eggs
+// Butter
+// Frozen Foods:
+
+// Frozen Fruits and Vegetables
+// Frozen Meals
+// Ice Cream
+// Frozen Pizza
+// Bakery:
+
+// Bread
+// Pastries
+// Cakes and Pies
+// Deli and Prepared Foods:
+
+// Sliced Meats and Cheeses
+// Salads
+// Ready-to-Eat Meals
+// Personal Care and Health:
+
+// Bath and Body Products
+// Hair Care
+// Oral Care
+// Skincare
+// Vitamins and Supplements
+// Household Supplies:
+
+// Cleaning Supplies
+// Laundry Detergent
+// Paper Products (Toilet Paper, Paper Towels)
+// Batteries
+// Baby and Child Care:
+
+// Diapers
+// Baby Food
+// Baby Care Products
+// Pet Supplies:
+
+// Dog Food
+// Cat Food
+// Pet Toys
+// Litter
+// Home and Kitchen:
+
+// Cookware
+// Small Appliances
+// Utensils and Cutlery
+// Kitchen Gadgets
