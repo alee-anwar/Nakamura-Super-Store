8e7553ac65a9faf785053aab80bd1e267fb91d11
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-15 16:49:35 +0500

    Update Layout.js

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -1,19 +1,10 @@
 import React from "react";
 // import { Outlet } from "react-router-dom";
-// import Appbar from "./components/Appbar/Appbar";
 import Appbar from "./Appbar/Appbar";
 import Navbar from "./Navbar/Navbar";
-// import { useLocation } from "react-router-dom";
 import { Box } from "@mui/material";
 
 export default function Layout({ children }) {
-  // const [title, setTitle] = useState(null);
-  // const location = useLocation();
-
-  // useEffect(() => {
-  //   const parsedTitle = location.pathname.replace(/\W/g, " ");
-  //   setTitle(parsedTitle);
-  // }, [location]);
 
   return (
     <Box sx={{ display: "flex" }}>
