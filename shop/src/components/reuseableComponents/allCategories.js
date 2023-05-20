9807228fe43e7bf29b53906d291b6bd61d13df37
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-20 17:47:21 +0500

    Create allCategories.js

---
+++ shop/src/components/reuseableComponents/allCategories.js
@@ -0,0 +1,74 @@
+export const categories = [
+  {
+    id: 0,
+    value: "Grocery",
+    label: "Grocery",
+    url: "https://envato-shoebox-0.imgix.net/4610/58d3-645e-4033-ba3f-e26b964bf07d/FM8_2374_sq.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1400&s=7699bbde755426700341f2400ed18129",
+    alt: "Female farmer holding basket with fresh vegetables, square crop",
+  },
+  {
+    id: 1,
+    value: "Bakery",
+    label: "Bakery",
+    url: "https://envato-shoebox-0.imgix.net/9d85/e546-12a2-43c6-8652-650d82e986db/2020_002_1448___2020-01-05+10.21.35.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=3965ec6a55eb850f590eac87e84a8d5e",
+    alt: "Top view on a assortment of loose eggs on a market, short circui",
+  },
+  {
+    id: 2,
+    value: "Meat & Seafood",
+    label: "Meat & Seafood",
+    url: "https://envato-shoebox-0.imgix.net/261a/1a57-852b-4f40-86d2-c2d1fc5c4f17/4V4A5614+copy.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=aa3c932b815cc0895c0570498b4b13ed",
+    alt: "Raw Fresh Marbled Meat Beef Steak",
+  },
+  {
+    id: 3,
+    value: "Dairy & Eggs",
+    label: "Dairy & Eggs",
+
+    url: "https://envato-shoebox-0.imgix.net/23ce/9483-c3da-42cd-990a-946f0c6b9aaf/sq_milk+pouring+into+glass+and+bowl+with+cornflakes+on+the+table.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=0c7de67778adc150d7ef8cbf49ee075f",
+    alt: "Milk Pouring Into Glass And Bowl With Cornflakes",
+  },
+  {
+    id: 4,
+    value: "Health & Care",
+    label: "Health & Care",
+    url: "https://images.pexels.com/photos/4055794/pexels-photo-4055794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+
+  {
+    id: 5,
+    value: "Household Items",
+    label: "Household Items",
+    url: "https://images.pexels.com/photos/15364240/pexels-photo-15364240/free-photo-of-items-in-a-shed.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+  },
+
+  {
+    id: 6,
+    value: "Home & Kitchen",
+    label: "Home & Kitchen",
+    url: "https://images.pexels.com/photos/5975/food-fashion-meal-white.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 7,
+    value: "Electronics",
+    label: "Electronics",
+  },
+  {
+    id: 8,
+    value: "Stationary",
+    label: "Stationary",
+    url: "https://images.pexels.com/photos/53874/pexels-photo-53874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+    id: 9,
+    value: "Snaks & Baverages",
+    label: "Snaks & Baverages",
+  },
+  {
+    id: 10,
+    value: "Pulses & Legumes",
+    label: "Pulses & Legumes",
+    url: "https://images.pexels.com/photos/618491/pexels-photo-618491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+];
