bfc526246f700fe5f89e31e0f98c92cb7baf9e24
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-05-20 17:46:37 +0500

    Update Transactions.js

--- admin/src/pages/Transactions.js
+++ admin/src/pages/Transactions.js
@@ -5,57 +5,85 @@ import { Box } from "@mui/system";
 // import { useNavigate } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
 import DotsMenuBtn from "../components/DotsMenuBtn";
-import { TapasTwoTone } from "@mui/icons-material";
+import { ConstructionOutlined, TapasTwoTone } from "@mui/icons-material";
+import moment from "moment";
+import axios from "axios";
 
-const Transactions = ({setTotalSales, totalSales}) => {
+const Transactions = ({ setTotalSales, totalSales }) => {
   const theme = useTheme();
   // const navigate = useNavigate();
   const [transactions, setTransactions] = useState([]);
+  const [deleted, setDeleted] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:3000/transactionList/viewTransaction")
       .then((res) => res.json())
       .then((data) => setTransactions(data));
-  }, [totalSales]);
+  }, [totalSales, deleted]);
 
-  // if(transactions) {
-  //   const totalAmount = 0;
-  //   totalAmount += totalSales;
-  //   setTotalSales(totalAmount)
-  // }
+  console.log(transactions.status);
+
+  useEffect(() => {
+    // Calculate total amount
+    const totalAmount = transactions.reduce(
+      (acc, transaction) => acc + transaction.amount,
+      0
+    );
+    setTotalSales(totalAmount);
+    console.log("Amount " + totalAmount);
+  }, [transactions, setTotalSales, deleted]);
+
+  // const handleDelete = async (id) => {
+  //   await fetch(
+  //     "http://localhost:3000/transactionList/deleteTransaction/" + id,
+  //     {
+  //       method: "DELETE",
+  //     }
+  //   );
+  //   const newTransactions = transactions.filter(
+  //     (transaction) => transaction._id !== id
+  //   );
+  //   setTransactions(newTransactions);
+  // };
 
   const handleDelete = async (id) => {
-    await fetch("http://localhost:3000/transactionList/deleteTransaction/" + id, {
-      method: "DELETE",
-    });
-    const newTransactions = transactions.filter((transaction) => transaction.id !== id);
+    await axios
+      .delete(`http://localhost:3000/transactionList/deleteTransaction/${id}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.message));
+    const newTransactions = transactions.filter(
+      (transaction) => transaction._id !== id
+    );
     setTransactions(newTransactions);
+    setDeleted(true);
   };
 
   const columns = [
-    { field: "id", headerName: "Transaction ID", width: 200 },
+    { field: "id", headerName: "Transaction ID", width: 150 },
+    { field: "amount", headerName: "Amount", width: 150 },
     { field: "status", headerName: "Status", width: 150 },
-    { field: "OID", headerName: "OID", width: 150 },
-    { field: "customerName", headerName: "Customer Name", width: 150 },
+    { field: "oid", headerName: "OID", width: 150 },
+    { field: "customerName", headerName: "Customer Name", width: 200 },
     // { field: "phone", headerName: "Phone#", width: 150 },
     { field: "date", headerName: "Date", width: 150 },
     {
       field: "action",
       headerName: "Action",
       renderCell: (params) => (
-        <DotsMenuBtn product={params.id} handleDelete={handleDelete}/>
+        <DotsMenuBtn product={params.id} handleDelete={handleDelete} />
       ),
       width: 150,
     },
   ];
   // const threedots = <MoreHorizRoundedIcon/>;
   const rows = transactions.map((row) => ({
-    id: row._id,
+    id: row.transactionId,
+    amount: row.amount,
     status: row.status,
-    oid: row.OID,
-    name: row.customerName,
-    // phone: row.phone,
-    date: row.date,
+    oid: row.oid,
+    customerName: row.customerName,
+    phone: row.phoneNo,
+    date: moment(row.date).format("DD/MM/YYYY"),
   }));
 
   // console.log(products);
@@ -67,9 +95,7 @@ const Transactions = ({setTotalSales, totalSales}) => {
         <Typography sx={{ flexGrow: 1 }} variant="h1">
           Transactions
         </Typography>
-        <Button variant="contained">
-          Add Transaction
-        </Button>
+        <Button variant="contained">Add Transaction</Button>
       </Box>
 
       <div style={{ height: "78vh", width: "100%" }}>
