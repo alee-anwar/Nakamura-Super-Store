2cb74d651df68c39e91c0bdcaa53e1e3f8ef5298
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-06-01 20:40:07 +0500

    Create Loading.js

---
+++ shop/src/components/Loading.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./custom.css";
+import { Box } from "@mui/material";
+import Logo from "../assets/bottle.gif";
+import ErrorMessage from "./ErrorMessage";
+
+const Loading = () => {
+  const [showNotFound, setShowNotFound] = useState(false);
+
+  useEffect(() => {
+    // Set a timeout to display "Product Not Found" after 3 seconds
+    const timeout = setTimeout(() => {
+      setShowNotFound(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        // flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60vh",
+        pb: 20,
+      }}
+    >
+        {showNotFound ? (
+          <Box pt={20}>
+            <ErrorMessage
+              path={"/shop"}
+              errorMessage={"Product Not Found"}
+              linkMsg={"Return to shopping"}
+            />
+          </Box>
+        ) : (
+          <Box
+            component="img"
+            src={Logo}
+            sx={{ height: "20vh", width: "auto" }}
+          />
+        )}
+      {/* <div  className="loading-animation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div > */}
+    </Box>
+  );
+};
+
+export default Loading;
