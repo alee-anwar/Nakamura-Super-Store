accf6a237d69c8f7ab932b969cbb94918c7e0860
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-06-01 20:39:13 +0500

    Update Shop.js

--- shop/src/pages/Shop.js
+++ shop/src/pages/Shop.js
@@ -15,6 +15,7 @@ import BreadcrumbsComponent from "../components/BreadcrumbsComponent";
 import ProductCard from "../components/ProductCard";
 // import FilterSidebar from "../components/FilterSidebar";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const Shop = ({
   cartItems,
@@ -28,6 +29,7 @@ const Shop = ({
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [error, setError] = useState(null);
   const [sortOrder, setSortOrder] = useState(""); // State for sorting order
+  const [loading, setLoading] = useState(true); // Loading state
 
   const location = useLocation(); // Get the current location
   const { value } = useParams(); // Get the product ID from the URL params
@@ -46,8 +48,10 @@ const Shop = ({
         );
         setProducts(response.data);
         console.log(response.data);
+        setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         setError(error.message);
+        setLoading(false); // Set loading to false if an error occurs
       }
     };
 
@@ -55,6 +59,7 @@ const Shop = ({
   }, []);
 
   console.log(products);
+
   useEffect(() => {
     console.log("category");
     // Filter the products based on the selected category (if any)
@@ -92,10 +97,6 @@ const Shop = ({
     );
   }, [sortOrder]);
 
-  // const handleSortChange = (event) => {
-  //   setSortOrder(event.target.value);
-  // };
-
   const handleSortChange = (event) => {
     const selectedSortOrder = event.target.value;
 
@@ -110,80 +111,86 @@ const Shop = ({
   return (
     <Container maxWidth="lg" disableGutters sx={{ pt: 5 }}>
       <BreadcrumbsComponent name={"Shop"} path={"/shop"} />
-      <Box display="flex" alignItems="center">
-        <Typography variant="h1" my={2} flexGrow={1}>
-          Shop
-        </Typography>
-        <Link to="#" onClick={handleGoBack} style={{ color: "#ffb800" }}>
-          Go Back
-        </Link>
-      </Box>
-      <Grid container columns={12} spacing={2} my={2}>
-        <Grid item xs={4} md={3} key="filter-sidebar">
-          <Paper
-            elevation={2}
-            style={{ padding: "16px", width: "auto", height: "auto" }}
-          >
-            <FormControl component="fieldset" p={2}>
-              <Typography variant="h6">Price</Typography>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Box display="flex" alignItems="center">
+            <Typography variant="h1" my={2} flexGrow={1}>
+              Shop
+            </Typography>
+            <Link to="#" onClick={handleGoBack} style={{ color: "#ffb800" }}>
+              Go Back
+            </Link>
+          </Box>
+          <Grid container columns={12} spacing={2} my={2}>
+            <Grid item xs={4} md={3} key="filter-sidebar">
+              <Paper
+                elevation={2}
+                sx={{ padding: "16px", width: "auto", height: "60vh"}}
+              >
+                <FormControl component="fieldset" p={2}>
+                  <Typography variant="h6">Price</Typography>
 
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={sortOrder === "lowToHigh"}
-                      onChange={handleSortChange}
-                      value="lowToHigh"
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={sortOrder === "lowToHigh"}
+                          onChange={handleSortChange}
+                          value="lowToHigh"
+                        />
+                      }
+                      label="Low to High"
                     />
-                  }
-                  label="Low to High"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={sortOrder === "highToLow"}
-                      onChange={handleSortChange}
-                      value="highToLow"
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={sortOrder === "highToLow"}
+                          onChange={handleSortChange}
+                          value="highToLow"
+                        />
+                      }
+                      label="High to Low"
                     />
-                  }
-                  label="High to Low"
-                />
-              </FormGroup>
-            </FormControl>
-          </Paper>
-        </Grid>
-        <Grid container item xs={8} md={9}>
-          <Grid container columns={12} spacing={1}>
-            {filteredProducts.length > 0
-              ? filteredProducts.map((item) => (
-                  <Grid item xs={6} sm={4} md={4} lg={3} key={item._id}>
-                    <ProductCard
-                      item={item}
-                      cartItems={cartItems}
-                      setCartItems={setCartItems}
-                      wishlist={wishlist}
-                      setWishlist={setWishlist}
-                      totalCost={totalCost}
-                      setTotalCost={setTotalCost}
-                    />
-                  </Grid>
-                ))
-              : products.map((item) => (
-                  <Grid item xs={6} sm={4} md={4} lg={3} key={item._id}>
-                    <ProductCard
-                      cartItems={cartItems}
-                      setCartItems={setCartItems}
-                      wishlist={wishlist}
-                      setWishlist={setWishlist}
-                      item={item}
-                      totalCost={totalCost}
-                      setTotalCost={setTotalCost}
-                    />
-                  </Grid>
-                ))}
+                  </FormGroup>
+                </FormControl>
+              </Paper>
+            </Grid>
+            <Grid container item xs={8} md={9}>
+              <Grid container columns={12} spacing={1}>
+                {filteredProducts.length > 0
+                  ? filteredProducts.map((item) => (
+                      <Grid item xs={6} sm={4} md={4} lg={3} key={item._id}>
+                        <ProductCard
+                          item={item}
+                          cartItems={cartItems}
+                          setCartItems={setCartItems}
+                          wishlist={wishlist}
+                          setWishlist={setWishlist}
+                          totalCost={totalCost}
+                          setTotalCost={setTotalCost}
+                        />
+                      </Grid>
+                    ))
+                  : products.map((item) => (
+                      <Grid item xs={6} sm={4} md={4} lg={3} key={item._id}>
+                        <ProductCard
+                          cartItems={cartItems}
+                          setCartItems={setCartItems}
+                          wishlist={wishlist}
+                          setWishlist={setWishlist}
+                          item={item}
+                          totalCost={totalCost}
+                          setTotalCost={setTotalCost}
+                        />
+                      </Grid>
+                    ))}
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
+        </>
+      )}
     </Container>
   );
 };
