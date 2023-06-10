ae64d376c08cc2e870e78e38033d479d10bb2610
alee-anwar <100070333+alee-anwar@users.noreply.github.com>
2023-06-10 14:55:10 +0500

    update

---
+++ admin/src/components/MicrosoftPowerBI.js
@@ -0,0 +1,44 @@
+// import React from 'react'
+// import { PowerBIEmbed } from 'powerbi-client-react';
+// import {models} from 'powerbi-client'
+
+// const MicrosoftPowerBI = () => {s
+//   return (
+//     <PowerBIEmbed
+// 	embedConfig = {{
+// 		type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+// 		id: '<Report Id>',
+// 		embedUrl: '<Embed Url>',
+// 		accessToken: '<Access Token>',
+// 		tokenType: models.TokenType.Embed, // Use models.TokenType.Aad for SaaS embed
+// 		settings: {
+// 			panes: {
+// 				filters: {
+// 					expanded: false,
+// 					visible: false
+// 				}
+// 			},
+// 			background: models.BackgroundType.Transparent,
+// 		}
+// 	}}
+
+// 	eventHandlers = {
+// 		new Map([
+// 			['loaded', function () {console.log('Report loaded');}],
+// 			['rendered', function () {console.log('Report rendered');}],
+// 			['error', function (event) {console.log(event.detail);}],
+// 			['visualClicked', () => console.log('visual clicked')],
+// 			['pageChanged', (event) => console.log(event)],
+// 		])
+// 	}
+
+// 	cssClassName = { "reportClass" }
+
+// 	getEmbeddedComponent = { (embeddedReport) => {
+// 		window.report = embeddedReport;
+// 	}}
+// />
+//   )
+// }
+
+// export default MicrosoftPowerBI
